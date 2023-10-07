39ad55a7690ed79b8c75149204988a9bc72bb70e
Ladol <41170848+Ladol@users.noreply.github.com>
2023-10-07 21:02:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 }
 # Define the URL to fetch JSON data from
-url = "https://www.infraestruturasdeportugal.pt/negocios-e-servicos/horarios-ncombio/730/2023-10-07"
+url = "https://www.infraestruturasdeportugal.pt/negocios-e-servicos/horarios-ncombio/731/2023-10-07"
 #just to force a push
 # Send an HTTP GET request to the URL
 response = requests.get(url, headers=headers)
@@ -20,7 +20,7 @@ if response.status_code == 200:
     file_path = f"2023-10/2023-10-07/730-2023-10-07.json"
 
     # Serialize the JSON data and save it to the specified file path
-    with open('output.json', 'w') as json_file:
+    with open('output2.json', 'w') as json_file:
         json.dump(data, json_file)
 
     print("JSON data has been fetched and saved successfully.")
